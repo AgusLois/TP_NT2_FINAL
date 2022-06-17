a61d482cc302c524a12b5c53d50a81db35053b2c
AgusLois <Agusslois98@gmail.com>
2022-06-17 17:08:29 -0300

    Login y registro

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="src-components-api-rest-ful">
+        <div class="jumbotron">
+            <h2>Hola mundo</h2>
+        <hr>
+        <hr>
+        <br>
+        </div>
+    </section>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+axios.defaults.baseURL = 'https://62842ba33060bbd3473556b1.mockapi.io/'
+
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
