905cb26b49010cc9fd0e1637a8a82dcf45ef0525
sofisrur <sofisrur1@gmail.com>
2022-06-19 17:43:59 -0300

    usuarios busqueda

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -1,22 +1,32 @@
 <template>
 
   <section class="src-components-usuario">
+    <hr>
     <h1>Listado de usuarios</h1>
+
     <button class="btn btn-success my-3 mr-3" @click="getPostsUsers()">Pedir usuarios</button>
-     <div v-if="usuarios.length" class="table-responsive">
-      <table class="table table-dark">
-        <tr>
-          <th>id</th>
-          <th>email</th>
-        </tr>
-        <tr v-for="(usuario, index) in usuarios" :key="index">
-          <td>{{ usuario.id }}</td>
-          <td>{{ usuario.email }}</td>
-        </tr>
-      </table>
-      <h4 class="alert alert-primary">Se encontraron {{usuarios.length}} usuarios</h4>
-      </div>
-      <h4 v-else class="alert alert-danger text-center">No se encontraron Usuarios</h4>
+    <button class="btn btn-danger my-3" @click="usuarios=[]">Cerrar</button>
+
+    <div class="container-fluid mt-3" id="app">
+    <input type="text" class="form-control" v-model="criterioDeBusqueda"
+        placeholder="Búsqueda de usuarios...">
+     <br>
+         <div v-if="usuarios.length" class="table-responsive">
+          <table class="table table-dark">
+              <tr>
+                 <th>id</th>
+                 <th>email</th>
+              </tr>
+              <tr v-for="(usuario,index) in personasFiltradas" :key="index">
+                 <td>{{ usuario.id }}</td>
+                 <td>{{ usuario.email }}</td>
+              </tr>
+          </table>
+           <h4 class="alert alert-primary">Se encontraron {{usuarios.length}} usuarios</h4>
+         </div>
+            <h4 v-else class="alert alert-danger text-center">No se encontraron Usuarios</h4>
+
+  </div>
   </section>
 
 </template>
@@ -32,20 +42,13 @@
     data () {
       return {
         usuarios:[],
+        criterioDeBusqueda: '',
         url:'https://62842ba33060bbd3473556b1.mockapi.io/users'
       }
     },
     methods: {
        async getPostsUsers(){
-   /*   
-      let {data} = await axios.get('/users').then(response =>{
-      console.log(response)
-    this.usuarios=data
-    }).catch(error=>{
-        console.log("Error Axios", error)
-      })
-    } */
-    try{
+      try{
       let {data} = await axios(this.url)
       this.usuarios=data
       }catch(error){
@@ -54,21 +57,20 @@
       }
     },
     computed: {
-      /*  try{
-      let {data} = await this.axios(this.url)
-      this.posts=data
-      }catch(error){
-         console.log("Error Axios", error)
-      } */
-
+      personasFiltradas() {
+        return this.usuarios.filter((usuario) => {
+         let registroCompleto = `${usuario.id} ${usuario.email}`
+         return registroCompleto.toLowerCase().includes(this.criterioDeBusqueda.toLowerCase())
+    });
     }
+  }
 }
 
 
 </script>
 
 <style scoped lang="css">
-  .src-components-usuario {
-
+  h1{
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   }
 </style>
